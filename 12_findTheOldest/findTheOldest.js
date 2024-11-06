72567b8448572a78311bf2e6e2a559c922c909b8
Abdullahmojumder <abdullah.mojumder@gmail.com>
2024-11-06 22:27:24 +0600

    function of finding the oldest.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const getAge = function(birth, death) {
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
 
+const findTheOldest = function people(){
+    return people.reduce((oldest, currentperson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(
+            currentperson.yearOfBirth,
+            currentperson.yearOfDeath
+        );
+    return oldestAge < currentAge ? currentperson : oldest;
+    });
 };
 
 // Do not edit below this line
